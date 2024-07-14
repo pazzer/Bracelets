1744125e7cc83716e6381ac86981f140e5675207
Paul Patterson <pazerp@hotmail.com>
2024-07-14 14:02:25 +0100

    Initial Commit

---
+++ Bracelets/BraceletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraceletsApp.swift
+//  Bracelets
+//
+//  Created by Paul Patterson on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BraceletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
